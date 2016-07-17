fdc0bbba31e4dd38f87a25a19cc3b62bf418969a
NCordon <nacho.cordon.castillo@gmail.com>
2016-07-17 16:49:07 +0200

    Añade documentación de solution.h

--- src/solution.h
+++ src/solution.h
@@ -4,23 +4,58 @@
 #include "aux.h"
 using namespace std;
 
+/**
+ * @class Solution
+ * @brief Stores a n-tuple
+ *
+ */
 class Solution: public std::vector<double>{
 private:
+    /*
+     * @var fvalue Stores the fitness of the position
+     */
     double fvalue;
 
 public:
+    /*
+     * Position constructor
+     * @param position n-tuple
+     */
     Solution(vector<double> position);
 
+    /*
+     * Position and fit constructor
+     * @param position n-tuple
+     * @param fitness of the tuple
+     */
     Solution(vector<double> position, double custom_fitness);
 
+    /*
+     * Empty default constructor
+     */
     Solution();
 
+    /*
+     * Copy constructor
+     * @param otro other solution tuple
+     */
     Solution(Solution const &otro);
 
+    /*
+     * Updates the fitness of the solution
+     */
     void updateFitness();
 
+    /*
+     * Set the fitness of the solution to a given value
+     * @param score fitness of the tuple
+     */
     void setFitness(double score);
 
+    /*
+     * Returns the fit of the solution
+     * @return fvalue the fitness value
+    */
     double getFitness();
 };
 
